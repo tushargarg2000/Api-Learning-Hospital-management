a47a1d3d56e2f5763070b8a31fd4e3cb5081031d
Tushar <tushargarg0110@gmail.com>
2024-04-17 23:02:43 +0530

    Added more APIs

--- src/main/java/com/acciojob/HospitalManagementSystemLearning/ApiClass.java
+++ src/main/java/com/acciojob/HospitalManagementSystemLearning/ApiClass.java
@@ -1,8 +1,10 @@
 package com.acciojob.HospitalManagementSystemLearning;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("patient")
 public class ApiClass {
 
     Map<Integer,Patient> patientDb = new HashMap<>();
@@ -49,5 +52,40 @@ public class ApiClass {
         return patient;
     }
 
+    @GetMapping("/getPatientByNameAndMobNo")
+    public Patient getPatientByNameAndMobNo(@RequestParam("name")String name,
+                                            @RequestParam("mobNo")String mobNo){
 
+        for(Patient patient: patientDb.values()) {
+            if(patient.getPatientName().equals(name) &&
+               patient.getMobNo().equals(mobNo)){
+                return patient;
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/findOldestPatientName")
+    public String findOldestPatient(){
+
+        //return name of the oldest patient
+        //present in the DB.
+        int maxAge = 0;
+        Patient ansPatient = null;
+        for(Patient patient: patientDb.values()){
+
+            if(patient.getPatientAge()> maxAge){
+                maxAge = patient.getPatientAge();
+                ansPatient = patient;
+            }
+        }
+        return ansPatient.getPatientName();
+    }
+
+    @DeleteMapping("deletePatientRecord")
+    public String deletePatientFromDb(@RequestParam("patientId")Integer patientId) {
+        patientDb.remove(patientId);
+        return "The patient has been removed from DB";
+    }
+    //Update mobile no. and Age
 }
