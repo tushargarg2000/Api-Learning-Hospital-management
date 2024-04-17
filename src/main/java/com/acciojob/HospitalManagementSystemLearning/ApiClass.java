e47a5062d483db4e164eac57dfae3368575d6762
Tushar <tushargarg0110@gmail.com>
2024-04-17 22:12:19 +0530

    Adding patient details

--- src/main/java/com/acciojob/HospitalManagementSystemLearning/ApiClass.java
+++ src/main/java/com/acciojob/HospitalManagementSystemLearning/ApiClass.java
@@ -1,13 +1,20 @@
 package com.acciojob.HospitalManagementSystemLearning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ApiClass {
 
-    @GetMapping("getWeatherInfo")
+    Map<Integer,Patient> patientDb = new HashMap<>();
+
+    @GetMapping("getWeatherInfo/today")
     public String getWeatherInformationPlusPlus() {
 
         return "The temperature is 30C, and feels like 35, the winds are" +
@@ -15,12 +22,31 @@ public class ApiClass {
     }
 
     @GetMapping("sum")
-    public String getSum(@RequestParam("no1")Integer no1,
-                         @RequestParam("no2")Integer no2){
+    public String getSum(@RequestParam("no1")int no1,
+                         @RequestParam("no2")int no2){
 
         Integer sum = no1+no2;
 
         return "The sum of numbers entered is "+sum;
+    }
+
+    @PostMapping("addPatient")
+    public String addPatient(@RequestBody Patient patient) {
+
+        int key = patient.getPatientId();
+        //Add this to the patient DB
+        patientDb.put(key,patient);
+
+        return "The patient with patientId"+key+" has been saved to DB";
+
+    }
+
+    @GetMapping("getPatientInfo")
+    public Patient getPatientInfo(@RequestParam("patientIdEnterInformation")Integer patientId){
+
+        //patientId is acting as the key
+        Patient patient = patientDb.get(patientId);
+        return patient;
     }
 
 
---
+++ src/main/java/com/acciojob/HospitalManagementSystemLearning/Patient.java
@@ -0,0 +1,50 @@
+package com.acciojob.HospitalManagementSystemLearning;
+
+public class Patient {
+
+    private int patientId;
+    private String patientName;
+    private int patientAge;
+    private String disease;
+    private String mobNo;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public int getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(int patientAge) {
+        this.patientAge = patientAge;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public String getMobNo() {
+        return mobNo;
+    }
+
+    public void setMobNo(String mobNo) {
+        this.mobNo = mobNo;
+    }
+}
